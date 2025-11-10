db97f592c874d07fb537413636885b4ce7bed971
luisguilhermeborges <borgesluisgb@gmail.com>
2025-11-10 15:42:58 -0300

    updateastro

--- site-cdc/astro.config.mjs
+++ site-cdc/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Adiciona esta linha
+  base: '/SITECDC', // Diga ao Astro que o site vive em /SITECDC/
+});
